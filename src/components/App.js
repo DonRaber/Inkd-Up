5f255cd3decfef6acd8fc6395d1c7f642b85e882
Donovan Raber <donovan.raber@outlook.com>
2023-11-15 11:46:05 -0800

    React Started

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+
+function App(){
+    return (
+        <div>
+            <h1>React Started</h1>
+        </div>
+    )
+}
+
+export default App
